a5e4f24ae613c97447817eeb7ba05bc8bd4b91c4
yohanleb <yohan.lebricquer@gmail.com>
2020-02-24 15:39:21 +0100

    Implementation of Mars Rover + tests

---
+++ src/main/java/com/esiea/tp4A/domain/MarsRoverImpl.java
@@ -0,0 +1,67 @@
+package com.esiea.tp4A.domain;
+
+public class MarsRoverImpl implements MarsRover {
+
+    private Integer x;
+    private Integer y;
+    private Direction direction;
+
+    public MarsRover initialize(Position position) {
+        this.x = position.getX();
+        this.y = position.getY();
+        this.direction = position.getDirection();
+        return this;
+    }
+
+    @Override
+    public Position move(String command) {
+        switch (command) {
+            case "f":
+                switch (this.direction) {
+                    case NORTH:
+                        return Position.of(this.x, this.y + 1, this.direction);
+                    case SOUTH:
+                        return Position.of(this.x, this.y - 1 , this.direction);
+                    case WEST:
+                        return Position.of(this.x + 1, this.y, this.direction);
+                    case EAST:
+                        return Position.of(this.x - 1, this.y, this.direction);
+                }
+            case "b":
+                switch (this.direction) {
+                    case NORTH:
+                        return Position.of(this.x, this.y - 1, this.direction);
+                    case SOUTH:
+                        return Position.of(this.x, this.y + 1, this.direction);
+                    case WEST:
+                        return Position.of(this.x - 1, this.y, this.direction);
+                    case EAST:
+                        return Position.of(this.x + 1, this.y, this.direction);
+                }
+            case "l":
+                switch (this.direction) {
+                    case NORTH:
+                        return Position.of(this.x, this.y, Direction.WEST);
+                    case SOUTH:
+                        return Position.of(this.x, this.y, Direction.EAST);
+                    case WEST:
+                        return Position.of(this.x, this.y, Direction.SOUTH);
+                    case EAST:
+                        return Position.of(this.x, this.y, Direction.NORTH);
+                }
+            case "r":
+                switch (this.direction) {
+                    case NORTH:
+                        return Position.of(this.x, this.y, Direction.EAST);
+                    case SOUTH:
+                        return Position.of(this.x, this.y, Direction.WEST);
+                    case WEST:
+                        return Position.of(this.x, this.y, Direction.NORTH);
+                    case EAST:
+                        return Position.of(this.x, this.y, Direction.SOUTH);
+                }
+            default:
+                return Position.of(this.x, this.y, this.direction);
+        }
+    }
+}
