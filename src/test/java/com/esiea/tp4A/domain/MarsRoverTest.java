b53bc9b08e8dd1eb69e72a5d58b4af8215d82241
yohanleb <yohan.lebricquer@gmail.com>
2020-02-24 15:39:21 +0100

    Implementation of Mars Rover + tests

--- src/test/java/com/esiea/tp4A/domain/MarsRoverTest.java
+++ src/test/java/com/esiea/tp4A/domain/MarsRoverTest.java
@@ -2,24 +2,31 @@ package com.esiea.tp4A.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MarsRoverTest {
 
-    @Test
-    void initialize() {
+    private final MarsRover rover = new MarsRoverImpl().initialize(Position.of(0, 0, Direction.NORTH));
 
+    @Test
+    void move_forward() {
+        Position newPosition = rover.move("f");
+
+        assertThat(newPosition)
+            .as("Rover position after f command")
+            .extracting(Position::getX, Position::getY, Position::getDirection)
+            .isEqualTo(List.of(0, 1, Direction.NORTH));
     }
 
     @Test
-    void updateMap() {
-    }
+    void move_backward() {
+        Position newPosition = rover.move("b");
 
-    @Test
-    void configureLaserRange() {
-    }
-
-    @Test
-    void move() {
+        assertThat(newPosition)
+            .as("Rover position after f command")
+            .extracting(Position::getX, Position::getY, Position::getDirection)
+            .isEqualTo(List.of(0, -1, Direction.NORTH));
     }
 }
