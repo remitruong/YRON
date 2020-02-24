5fe91e98b02575505547a015e2b64dc54988d791
Doremi <remi.truong98@gmail.com>
2020-02-24 15:55:43 +0100

    Add multiple command test and unknown command test

--- src/test/java/com/esiea/tp4A/domain/MarsRoverTest.java
+++ src/test/java/com/esiea/tp4A/domain/MarsRoverTest.java
@@ -29,4 +29,47 @@ class MarsRoverTest {
             .extracting(Position::getX, Position::getY, Position::getDirection)
             .isEqualTo(List.of(0, -1, Direction.NORTH));
     }
+
+    @Test
+    void move_left(){
+        Position newPosition = rover.move("l");
+
+        assertThat(newPosition)
+            .as("Rover position after l command")
+            .extracting(Position::getX, Position::getY, Position::getDirection)
+            .isEqualTo(List.of(0,0,Direction.WEST));
+    }
+
+    @Test
+    void move_right(){
+        Position newPosition = rover.move("r");
+
+        assertThat(newPosition)
+            .as("Rover position after r command")
+            .extracting(Position::getX, Position::getY, Position::getDirection)
+            .isEqualTo(List.of(0, 0, Direction.EAST));
+    }
+
+    @Test
+    void move_with_multiple_command(){
+        Position newPosition = rover.move("f");
+        newPosition = rover.move("f");
+        newPosition = rover.move("l");
+        newPosition = rover.move("b");
+
+        assertThat(newPosition)
+            .as("Rover position after f,f,l,b command")
+            .extracting(Position::getX, Position::getY, Position::getDirection)
+            .isEqualTo(List.of(1, 2, Direction.WEST));
+    }
+
+    @Test
+    void move_with_unknown_command(){
+        Position newPosition = rover.move("g");
+
+        assertThat(newPosition)
+            .as("Rover position after g command")
+            .extracting(Position::getX, Position::getY, Position::getDirection)
+            .isEqualTo(List.of(0, 0, Direction.NORTH));
+    }
 }
