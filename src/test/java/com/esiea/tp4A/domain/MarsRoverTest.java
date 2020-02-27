e0b9836a4cdda53e8e7c8b89d3146c2628fbd0b1
KANTAR <osman.kantar@atos.net>
2020-02-27 21:38:49 +0100

    ajout de tests de directions pour commande f

--- src/test/java/com/esiea/tp4A/domain/MarsRoverTest.java
+++ src/test/java/com/esiea/tp4A/domain/MarsRoverTest.java
@@ -9,7 +9,40 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MarsRoverTest {
 
     private final MarsRoverImpl rover = (MarsRoverImpl) new MarsRoverImpl().initialize(Position.of(0, 0, Direction.NORTH));
+    private final MarsRoverImpl rover2 = (MarsRoverImpl) new MarsRoverImpl().initialize(Position.of(1, 1, Direction.SOUTH));
+    private final MarsRoverImpl rover3 = (MarsRoverImpl) new MarsRoverImpl().initialize(Position.of(1, 1, Direction.EAST));
+    private final MarsRoverImpl rover4 = (MarsRoverImpl) new MarsRoverImpl().initialize(Position.of(1, 1, Direction.WEST));
+    
+    @Test
+    void move_forward2() {
+        Position newPosition = rover2.move("f");
 
+        assertThat(newPosition)
+            .as("Rover position after f command")
+            .extracting(Position::getX, Position::getY, Position::getDirection)
+            .isEqualTo(List.of(1, 0, Direction.SOUTH));
+    }
+    
+    @Test
+    void move_forward3() {
+        Position newPosition = rover3.move("f");
+
+        assertThat(newPosition)
+            .as("Rover position after f command")
+            .extracting(Position::getX, Position::getY, Position::getDirection)
+            .isEqualTo(List.of(2, 1, Direction.EAST));
+    }
+    
+    @Test
+    void move_forward4() {
+        Position newPosition = rover4.move("f");
+
+        assertThat(newPosition)
+            .as("Rover position after f command")
+            .extracting(Position::getX, Position::getY, Position::getDirection)
+            .isEqualTo(List.of(0, 1, Direction.WEST));
+    }
+    
     @Test
     void move_forward() {
         Position newPosition = rover.move("f");
